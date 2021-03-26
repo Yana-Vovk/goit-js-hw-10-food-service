9e6df0bb76894ba93cd8b616cd6379fe52e3c75f
Yana Vovk <yanavovk1990@outlook.com>
2021-03-26 09:28:36 +0200

    Добавляет код 10 д.з.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import './styles.css';
+import menuItems from './menu.json';
+import menuTpl from './templates/menu.hbs';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const menuMarkup = createMenuMarkup(menuItems);
+const menuListRef = document.querySelector('.js-menu');
+const switchRef = document.querySelector('#theme-switch-toggle');
+const bodyRef = document.querySelector('body');
+
+menuListRef.insertAdjacentHTML('beforeend', menuMarkup);
+
+bodyRef.classList.add(localStorage.getItem('bodyTheme'));
+
+if (localStorage.getItem('bodyTheme') === 'dark-theme') {
+    switchRef.checked = true;
+}
+
+switchRef.addEventListener('change', onSwitchBtnChange);
+
+function createMenuMarkup(menuItems) {
+    return menuItems.map(menuTpl).join('');
+}
+
+function onSwitchBtnChange() {
+    if (switchRef.checked === true) {
+        classReplaceFunc(Theme.DARK);
+    } else {
+        classReplaceFunc(Theme.LIGHT);
+    }
+}
+
+function classReplaceFunc(theme) {
+    bodyRef.classList.value = '';
+    bodyRef.classList.add(theme);
+    localStorage.setItem('bodyTheme', theme);
+}
